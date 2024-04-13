ab09192827f147db5e2138a776e1d2f5c715c1a1
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-04-13 23:23:03 +0100

    Remove the runtime output.

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            long startTime = System.nanoTime();
-            GameMap map = new GameMap("puzzle_160.txt");
+            GameMap map = new GameMap("puzzle_40.txt");
             PathFinder finder = new PathFinder(map);
             String path = finder.findPath();
             System.out.println(path);
-            long endTime = System.nanoTime();
-            // Calculate the elapsed time
-            long elapsedTime = endTime - startTime;
-            // Convert nanoseconds to milliseconds for readability
-            double milliseconds = (double) elapsedTime / 1_000_000;
-
-            // Print the elapsed time
-            System.out.println("Elapsed Time: " + milliseconds + " milliseconds");
 
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
